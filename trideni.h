74cede6905d6ba61e7b5f6ab5c69e54e25e47178
vrbattom <71826553+vrbattom@users.noreply.github.com>
2020-10-22 14:13:01 +0200

    Update trideni.h

--- trideni.h
+++ trideni.h
@@ -8,6 +8,8 @@ typedef std::vector<std::string> seznam_slov;
 
 void nacti (std::string nazev, seznam_slov & jmena);
 
-int zeptej_se_jestli_vypsat();
+bool zeptej_se_jestli_vypsat();
 	
-void vypis(seznam_slov & serazeni);
+void serad(seznam_slov & jmena);
+
+void vypis(seznam_slov const& jmena);
